e3d06fe9b6a9504a86bba6aa87eb3384b1e2338e
mankec <milan.kecman270@gmail.com>
2022-07-23 12:22:15 +0200

    Changed path of src to

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { sveltekit } from '@sveltejs/kit/vite'
+import path from 'path'
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
-};
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			$root: path.resolve('./src')
+		}
+	}
+}
 
-export default config;
+export default config
