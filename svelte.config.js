49de48c9578b172dd3b3777d238ab3285bee2648
mankec <milan.kecman270@gmail.com>
2022-08-22 20:15:54 +0200

    Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-// import adapter from '@sveltejs/adapter-auto'
+// import vercel from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess'
-import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,16 +9,7 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    adapter: adapter({
-      // if true, will create a Netlify Edge Function rather
-      // than using standard Node-based functions
-      edge: false,
-
-      // if true, will split your app into multiple functions
-      // instead of creating a single one for the entire app.
-      // if `edge` is true, this option cannot be used
-      split: false
-    }),
+    adapter: adapter(),
     paths: {
       base: ''
     }
