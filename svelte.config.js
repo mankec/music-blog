6078a5df53284871484b8d77d1254b0137e9f063
mankec <milan.kecman270@gmail.com>
2022-08-22 16:18:46 +0200

    netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-// import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess'
-import adapter from '@sveltejs/adapter-netlify';
+// import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,14 +10,14 @@ const config = {
 
   kit: {
     adapter: adapter({
-      // if true, will create a Netlify Edge Function rather
-      // than using standard Node-based functions
-      edge: false,
+      // // if true, will create a Netlify Edge Function rather
+      // // than using standard Node-based functions
+      // edge: false,
 
-      // if true, will split your app into multiple functions
-      // instead of creating a single one for the entire app.
-      // if `edge` is true, this option cannot be used
-      split: false
+      // // if true, will split your app into multiple functions
+      // // instead of creating a single one for the entire app.
+      // // if `edge` is true, this option cannot be used
+      // split: false
     }),
     paths: {
       base: '/mb'
