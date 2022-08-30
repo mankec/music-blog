8e167854cc59364549dca5847f9a366628c01e0b
mankec <milan.kecman270@gmail.com>
2022-08-30 16:44:38 +0200

    test

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,15 +1,5 @@
 /// <reference types="@sveltejs/kit" />
 
 declare namespace App {
-  // interface Locals {
-  // 	user?: { username: string }
-  // }
 
-  // interface Platform {}
-
-  // interface Session {
-  // 	user?: { username: string }
-  // }
-
-  // interface Stuff {}
 }
